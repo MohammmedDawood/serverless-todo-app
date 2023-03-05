345510a3462f7f893d0053211e884792cb066ff1
Mohammed Dawood <mahmedtony73@gmail.com>
2023-03-05 13:38:36 +0200

    add updateTodo function

--- backend/src/businessLogic/todos.ts
+++ backend/src/businessLogic/todos.ts
@@ -2,7 +2,7 @@ import { TodosAccess } from '../dataLayer/todosAcess'
 import { AttachmentUtils } from '../helpers/attachmentUtils'
 import { TodoItem } from '../models/TodoItem'
 import { CreateTodoRequest } from '../requests/CreateTodoRequest'
-// import { UpdateTodoRequest } from '../requests/UpdateTodoRequest'
+import { UpdateTodoRequest } from '../requests/UpdateTodoRequest'
 import { createLogger } from '../utils/logger'
 import * as uuid from 'uuid'
 // import * as createError from 'http-errors'
@@ -39,4 +39,15 @@ export async function createTodo(
   }
 
   return todosAccess.createTodoItem(newItem)
+}
+
+// update todo function
+export async function updateTodo(
+  userId: string,
+  todoId: string,
+  updatedTodo: UpdateTodoRequest
+): Promise<UpdateTodoRequest> {
+  logger.info('updateTodo', { userId, todoId, updatedTodo })
+
+  return todosAccess.updateTodoItem(userId, todoId, updatedTodo)
 }
