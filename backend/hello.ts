18646efbad1b24db3d0d24d7591c7af5d5d98576
Mohammed Dawood <mahmedtony73@gmail.com>
2023-03-05 07:48:24 +0200

    first hello commit

---
+++ backend/hello.ts
@@ -0,0 +1,15 @@
+import 'source-map-support/register'
+
+export const handler = async (event: any) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Go Serverless v1.0! Your function executed successfully!',
+        input: event
+      },
+      null,
+      2
+    )
+  }
+}
